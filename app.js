144984af6ec078e482fa32da7c6ff64945c6bb3e
Nicolas Ramirez <nico9017@gmail.com>
2022-02-01 17:49:48 +0100

    adding build view and public folder

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 
 function setup(app) {
+  const staticsFolder = path.join(__dirname, 'public');
   app.use(morgan('dev'));
   app.use('/api/artists', require('./controllers/artists'));
+
+  app.use(express.static(staticsFolder));
+  app.get('/index.html', (req, res) => {
+      res.sendFile(path.join(staticsFolder, '/index.html'));
+  });
 
   app.use((error, req, res, next) => {
     console.error(error);
 
     res.send(400);
-  })
+  });
 }
 
 const init = async (port) => {
